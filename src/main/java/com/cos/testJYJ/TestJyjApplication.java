cbd589f82e35b22184fde0d00577acee9526994e
ssar <ssar@nate.com>
2021-09-06 16:32:21 +0900

    환경설정

---
+++ src/main/java/com/cos/testJYJ/TestJyjApplication.java
@@ -0,0 +1,13 @@
+package com.cos.testJYJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJyjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJyjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/testJYJ/TestJyjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.testJYJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJyjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
